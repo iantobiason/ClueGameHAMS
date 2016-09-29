dca38a9c173f22475ea51d754a91a852457905c9
Michael Szostak <mszostak@mymail.mines.edu>
2016-09-29 16:48:37 -0600

    "added src/"

---
+++ src/experiment/BoardCell.java
@@ -0,0 +1,24 @@
+package experiment;
+
+public class BoardCell {
+		private int row;
+		private int column;
+		public BoardCell(int row, int column) {
+			super();
+			this.row = row;
+			this.column = column;
+		}
+		public int getRow() {
+			return row;
+		}
+		public void setRow(int row) {
+			this.row = row;
+		}
+		public int getColumn() {
+			return column;
+		}
+		public void setColumn(int column) {
+			this.column = column;
+		}
+
+}
---
+++ src/experiment/IntBoard.java
@@ -0,0 +1,49 @@
+package experiment;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class IntBoard {
+	// Double array grid of type BoardCell (will have row and column ints for each point)
+	private BoardCell[][] grid;
+	
+	//Set of Boardcells for visited and Targets so that...
+	private Set<BoardCell> visited;
+	private Set<BoardCell> targets;
+	// Store the adjacency lists in a HashMap of Key: BoardCell, and Value: Set<BoardCell>
+	private Map<BoardCell, Set<BoardCell>> adjacentMatrix;
+	
+	//CONSTRUCTOR - TO MODIFY
+	public IntBoard(BoardCell[][] grid, Set<BoardCell> visited, Set<BoardCell> targets,
+			HashMap<BoardCell, Set<BoardCell>> adjacentMatrix) {
+		super();
+		this.grid = grid;
+		this.visited = visited;
+		this.targets = targets;
+		this.adjacentMatrix = adjacentMatrix;
+	}
+	
+	// Calculates the adjacency list for each grid cell, stores in a map data structure. If you don't know what this is, go back to Preparation.
+	public Map<BoardCell, Set<BoardCell>> calcAdjacencies(){
+		return null;
+	}
+	
+	//Calculates the targets that are pathLength distance from the startCell. The list of targets will be stored in an instance variable.
+	// PLACE ALGORYTHM HERE
+	public Set<BoardCell> calcTargets(BoardCell startCell, int pathLength){
+		return null;
+	}
+	
+	// Returns the list of targets. The return type is Set<BoardCell>.
+	public Set<BoardCell> getTargets(){
+		return null;
+	}
+	
+	//Returns the adjacency list for one cell, type is Set<BoardCell>
+	public Set<BoardCell> getAdjList(){
+		return null;
+	}
+	
+	
+}
