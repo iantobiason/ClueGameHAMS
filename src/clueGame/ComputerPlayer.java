2e1ac3f9d56bec6c1c574a92351af59ec43a49a7
Ian Tobiason <itobiaso@adit.mines.edu>
2016-10-20 11:19:00 -0600

    Created four new classes for player functionality

---
+++ src/clueGame/ComputerPlayer.java
@@ -0,0 +1,20 @@
+package clueGame;
+
+import java.util.Set;
+
+public class ComputerPlayer {
+	
+	public ComputerPlayer() {
+		// TODO Auto-generated constructor stub
+	}
+	public BoardCell pickLocation(Set<BoardCell> targets) {
+		return null;
+	}
+	public void makeAccusation() {
+		
+	}
+	public void createSuggestion() {
+		
+	}
+
+}
---
+++ src/clueGame/Solution.java
@@ -0,0 +1,12 @@
+package clueGame;
+
+public class Solution {
+	private String person;
+	private String room;
+	private String weapon;
+	
+	public Solution() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
