14ea8482408a93445ec56b3d482fe454bf802ea3
Christian McErlean <cmcerlea@mines.edu>
2016-10-20 11:23:56 -0600

    Added CardType and Card classes. Also added new methods to board

---
+++ src/clueGame/Card.java
@@ -0,0 +1,20 @@
+package clueGame;
+
+public class Card {
+
+	private String cardName;
+	private CardType type;
+	
+	public Card(String name, CardType cardType) {
+		cardName = name;
+		type = cardType;
+	}
+
+	public boolean equals(Card card){
+		return cardName.equals(card.getName());
+	}
+	
+	public String getName(){
+		return cardName;
+	}
+}
